68d5c67e0ad9ca72a768f03ab441ad27d44bfd40
vhag233392 <233392@ids.upchiapas.edu.mx>
2024-09-11 14:54:11 -0600

    dev

--- src/app/users/users-dashboard/users-dashboard.component.ts
+++ src/app/users/users-dashboard/users-dashboard.component.ts
@@ -1,41 +1,42 @@
 import { Component } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { IUser } from '../modelo/iuser';
-import {MatTableModule} from '@angular/material/table';
 
 @Component({
   selector: 'app-users-dashboard',
   templateUrl: './users-dashboard.component.html',
-  styleUrl: './users-dashboard.component.css'
+  styleUrls: ['./users-dashboard.component.css']
 })
 export class UsersDashboardComponent {
 
-  users_list: IUser[] = []
-
-  selected_user:IUser = {
+  users_list: IUser[] = [];
+  selected_user: IUser = {
     id: 1,
     name: "Usuario 1",
     username: "user_1",
     phone: "19997355272",
     website: "akjhbsasays"
+  };
+
+  message: string = "Hola Mundo";
+
+  constructor(private _service: UserService) {
+    this.loadUsers();
   }
 
-  message: string = "Hola Mundo"
-
-  constructor(private _service: UserService){
-
-    this._service.getAll().subscribe(
+  // Cargar usuarios combinados (API + localStorage)
+  loadUsers(): void {
+    this._service.getAllUsers().subscribe(
       response => this.users_list = response
-    )
+    );
+  }
 
-   }
-
-   seleccionar_usuario(user: IUser): void{
+  seleccionar_usuario(user: IUser): void {
     this.selected_user = user;
-   }
+  }
 
-   eventoRecibido(message: string){
-    this.message = message
-   }
-
+  eventoRecibido(message: string): void {
+    this.message = message;
+    this.loadUsers(); // Recargar los usuarios después de añadir uno nuevo
+  }
 }
